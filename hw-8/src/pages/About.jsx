d48634973a11dfce0bd9743048a82e65a70a3d50
AidaDaiyrbekova <hgirl0108@gmail.com>
2025-08-27 23:33:17 +0600

    hw8

--- hw-8/src/pages/About.jsx
+++ hw-8/src/pages/About.jsx
@@ -3,10 +3,10 @@ import styles from './About.module.css'
 export const About = () => {
   return (
     <div className={styles.about}>
-      <h2>О проекте</h2>
+      <h2>About the project</h2>
       <img src="https://www.shkolazhizni.ru/img/content/i160/160260_or.jpg" alt="post" />
-      <p>Это простое SPA-приложение, созданное на React с использованием React Router Dom.</p>
-      <p>Вы можете просматривать посты и добавлять свои собственные.</p>
+      <p>This is a simple SPA application built in React using React Router Dom.</p>
+      <p>You can view posts and add your own.</p>
     </div>
   )
 }
--- hw-8/src/pages/Home.jsx
+++ hw-8/src/pages/Home.jsx
@@ -4,9 +4,9 @@ import styles from './Home.module.css'
 export const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Добро пожаловать в наш Блог!</h1>
+      <h1>Welcome to our Blog!</h1>
       <img src="https://onlypult.com/blog_uploads/9f7f6ead397f623793c262c8ce562c97.png" alt="post" />
-      <p>Читайте интересные статьи, делитесь мыслями, создавайте новые посты и оставайтесь с нами.</p>
+      <p>Read interesting articles, share your thoughts, create new posts and stay with us.</p>
     </div>
   )
 }
